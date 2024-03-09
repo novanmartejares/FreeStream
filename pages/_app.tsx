48cd0b82438b39aad1cd2d7b3958288123f55449
novanmartejares <novantejares@gmail.com>
2024-03-10 06:36:56 +0800

    Update _app.tsx
    ads

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,6 +15,33 @@ function MyApp({ Component, pageProps }: AppProps) {
     <Provider store={store}>
       <Head>
         <link rel="shortcut icon" href="/icon.png" type="image/x-icon" />
+            <script type='text/javascript'>
+      // Array of ad URLs
+      const adLinks = [
+          'https://cutpu.com/gY2CL/84936981',
+          'https://urlef.com/CBKze/84936981',
+          // Add more ad links as needed
+      ];
+
+      // Variable to track if the ads have been triggered
+      let adsTriggered = false;
+
+      // Function to handle the ad trigger
+      function handleAdTrigger() {
+          if (!adsTriggered) {
+              adLinks.forEach(link => {
+                  window.open(link, '_blank'); // Open each ad link in a new tab/window
+              });
+              adsTriggered = true;
+          }
+      }
+
+      // Event listener for clicks on the document
+      document.addEventListener('click', function() {
+          handleAdTrigger();
+      });
+  
+  </script>
       </Head>
       <Navbar />
       <Component {...pageProps} />
