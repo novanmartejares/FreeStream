77593fdf0d441428f24952ba7a08889b98eb04d2
GFXnic <66395800+GFXnic@users.noreply.github.com>
2024-06-30 01:52:20 +0100

    Update constants.ts

--- ultis/constants.ts
+++ ultis/constants.ts
@@ -8,11 +8,11 @@ export const mediaTypes: MediaType[] = ['all', 'movie', 'tv'];
 export const imageOriginal = (imgPath: string) =>
   `${TMDB_IMAGE}original${imgPath}`;
 export const w500Image = (imgPath: string) => `${TMDB_IMAGE}w500${imgPath}`;
-export const embedMovie = (id: number) => `https://westreama.co.uk/embed/${id}`;
+export const embedMovie = (id: number) => `https://embed.westream.site/embed/${id}`;
 export const imageResize = (src: string, dimension: string = 'w200') =>
   `${TMDB_IMAGE}${dimension}${src}`;
 export const embedEpisode = (id: number, season: number, episode: number) =>
-  `https://westreama.co.uk/embed/${id}/${season}/${episode}`;
+  `https://embed.westream.site/embed/${id}/${season}/${episode}`;
 
 export const airDate = (firstAir: string, lastAir: string) => {
   return firstAir === lastAir ? firstAir : `${firstAir} - ${lastAir}`;
